0361d7b526611fc0e18689da3a86b154fc926b54
Vshishkar <vshishkarov@gmail.com>
2018-04-29 21:22:25 +0300

    Add simple validation

--- task01/src/by/tc/task01/service/validation/Validator.java
+++ task01/src/by/tc/task01/service/validation/Validator.java
@@ -3,12 +3,18 @@ package by.tc.task01.service.validation;
 import by.tc.task01.entity.criteria.Criteria;
 
 public final class Validator {
-
-	public static boolean criteriaValidator(Criteria criteria){
-		return true;
+	
+	public static <E> boolean criteriaValidator(Criteria<E> criteria){
 		
+		Set<E> keys = criteria.keySet();
+		
+		for(E key : keys){	
+			if(criteria.getValue(key) == null){
+				return false;
+			}
+		}
+		 return true;				
 	}
 	
 	private Validator(){}
-	
 }
